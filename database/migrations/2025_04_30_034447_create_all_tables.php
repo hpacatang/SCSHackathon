809f37254d12f492cb95dd169b55853d688b2335
hpacatang <hadrielpacatang@gmail.com>
2025-04-30 15:07:47 +0800

    some changes

--- database/migrations/2025_04_30_034447_create_all_tables.php
+++ database/migrations/2025_04_30_034447_create_all_tables.php
@@ -6,22 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('port', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('port');
+       
     }
 };
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;                
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+//shipment id, products, time, origin, destination, status, riskLevel, trackerID, inventoryID
